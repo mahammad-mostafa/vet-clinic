f792ae360491e07e48bcfd1721856c2692d00cc4
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-09-26 22:01:51 +0300

    Insertion queries for filling the table

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '2020-2-3', 0, 'TRUE', 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15', 2, 'TRUE', 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-1-7', 1, 'FALSE', 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', '2017-5-12', 5, 'TRUE', 11);
